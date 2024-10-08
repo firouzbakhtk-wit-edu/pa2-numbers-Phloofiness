d7ef7becc294b986af3470c9cd22b4feab93f929
Holden Marnell <marnellh@wit.edu>
2024-10-08 16:01:26 -0400

    Submission for Assignment PA2a and PA2b.

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,95 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ * Solution to the first part of the second programming assignment.
+ * 
+ * This code should prompt the user to input 5 different whole numbers (Integers) at the same time, to which the system will store
+ * in an array list and then calculate the sums and averages of the positive and negative numbers, as well as the two lists combined.
+ * 
+ * @author Holden Marnell
+ */
 public class PA2a {
+	/**
+	 * This will enable the program, prompting the user to input the 5 numbers used to calculate the sums and averages,
+	 * as well as declare the arraylists and call the methods.
+	 * 
+	 * @param args
+	 */
+	
+	// Declares the ArrayLists posnum and negnum
+    static ArrayList<Integer> posnum = new ArrayList<>();
+    static ArrayList<Integer> negnum = new ArrayList<>();
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+    public static void main(String[] args) {
+    	// clears the ArrayLists after every use.
+    	posnum.clear();
+    	negnum.clear();
+    	
+    	// Calls the scanner method (method used to input the numbers and sort them)
+    	// calls the sumandaverage method (method used to calculate the sum and averages of the numbers)
+        scanner();
+        sumandaverage();
+    }
 
+    // declares the method scanner.
+    public static void scanner() {
+    	//declares the scanner and asks the user to input 5 whole numbers, and after input they will be sorted out into negnum and posnum.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter five whole numbers: ");
+        for (int i = 0; i < 5; i++) {
+            int number = scanner.nextInt();
+            if (number > 0) {
+                posnum.add(number);
+            } else if (number <= 0) {
+                negnum.add(number);
+            }
+        }
+    };
+    // declares the method sumandaverage.
+    private static void sumandaverage() {
+    	// declares variable sumPos, which is used to calculate the average of the positive numbers for every number in posnum.
+        int sumPos = 0;
+        for(int number : posnum) {
+            sumPos += number;
+        }
+        // prints the output of sumPos
+        System.out.println("The sum of the "+ posnum.size() + " positive " + (posnum.size() == 1 ? "number: " : "numbers: ") + sumPos);
+
+    	// declares variable sumNeg, which is used to calculate the average of the negative numbers for every number in negnum.
+        int sumNeg = 0;
+        for(int number : negnum) {
+            sumNeg += number;
+        }
+        // prints the output of negPos
+        System.out.println("The sum of the "+ negnum.size() + " non-positive " + (negnum.size() == 1 ? "number: " : "numbers: ") + sumNeg);
+
+        // Adds both of the sums together to get the total sum of the 5 numbers (sumTotal) and then prints it.
+        int sumtotal = sumPos + sumNeg;
+        System.out.println("The sum of the 5 numbers: " + sumtotal);
+
+        // Takes the sum of all the positive numbers and divides it by the amount of numbers in posnum. If the output is "NaN", it will print out 0.00
+        double avgPos = (double) sumPos / posnum.size();
+        if(Double.isNaN(avgPos)) {
+        	avgPos = 0.00;
+        }
+        // Prints the result.
+        System.out.printf("The average of the "+ posnum.size() + " positive " + (posnum.size() == 1 ? "number: " : "numbers: ") + "%.2f%n", avgPos);
+
+        // Takes the sum of all the negative numbers and divides it by the amount of numbers in negnum. If the output is "NaN", it will print out 0.00
+        double avgNeg = (double) sumNeg / negnum.size();
+        if(Double.isNaN(avgNeg)) {
+        	avgNeg = 0.00;
+        }
+        // Prints the result.
+        System.out.printf("The average of the "+ negnum.size() + " non-positive " + (negnum.size() == 1 ? "number: " : "numbers: ") + "%.2f%n", avgNeg);
+
+        // Takes the sum of all the numbers and divides them by 5, if the result if "NaN" the output will be 0.00
+        double avgTotal = (double) sumtotal / 5;
+        if(Double.isNaN(avgTotal)) {
+        	avgTotal = 0.00;
+        }
+        // Prints the result.
+        System.out.printf("The average of the 5 numbers: %.2f%n", avgTotal);
+    }
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,48 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
+/**
+ * Solution to the second part of the second programming assignment.
+ * 
+ * This code should prompt the user to input floats a b and c for the discriminant of the quadratic formula.
+ * Once input, the code should be able to find whether there are two roots, one root if it is equal to zero, or if the roots are imaginary.
+ * 
+ * @author Holden Marnell
+ */
 public class PA2b {
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+	/**
+	 * This will enable the program, prompting the user to input the numbers used for the conversion.
+	 * 
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	// Opens a scanner and prompts the user to input floats a b and c.
+        Scanner scanner = new Scanner(System.in);
 
+        // Declaring variables a b and c
+        System.out.print("Enter a b c: ");
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+        float c = scanner.nextFloat();
+
+        // Finding d, which is the discriminant of the quadratic formula.
+        float d = ((b*b) - 4*a*c);
+
+        // Checks if the discriminant is greater than, equal too, or less than 0. Depending on this output, the code will either
+        // declare the roots are imaginary, declare that there is only one root (variable e), or find that there is two roots that can be calculated.
+        if(d < 0){
+        	// Prints imaginary if d is less than 0.
+            System.out.println("Roots: imaginary");
+        } else if(d == 0){
+        	// If d == 0, it will calculate a special equation to make variable e, which will then be output to the user as one root.
+            float e = (-b/2*a);
+            System.out.printf("Root: %.2f%n", e);
+        } else if(d > 0){
+        	// If d is greater than 0, the code will find the roots for both the positive and negative parts of the quadratic formula, and then sort them from smallest to greatest.
+            double r1 = (-b + Math.sqrt(d))/(2*a);
+            double r2 = (-b - Math.sqrt(d))/(2*a);
+            System.out.printf("Roots: %.2f, %.2f%n", Math.min(r1, r2), Math.max(r1,r2));
+        }
+    }
 }
+
